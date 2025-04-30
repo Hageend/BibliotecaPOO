b7e1b3dbb573989172837e2b053e015ee905772b
Andy20s-75 <anaandread408@gmail.com>
2025-04-30 15:31:07 -0600

    se agrego la clase base

--- bibliotecapoo/src/main/java/com/bibliotecapoo/Main.java
+++ bibliotecapoo/src/main/java/com/bibliotecapoo/Main.java
@@ -5,3 +5,22 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+//Clase base
+public abstract class RecursoBibliografico {
+    private String titulo;
+
+    public RecursoBibliografico(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public abstract void mostrarDetalle();
+}
