c6d24bf0bc39df5ee63e684bf0668b82df32b45c
Andy20s-75 <anaandread408@gmail.com>
2025-04-30 15:56:26 -0600

    Se agrego la clase abstracta y la subclase libro en la herencia

--- bibliotecapoo/src/main/java/com/bibliotecapoo/Main.java
+++ bibliotecapoo/src/main/java/com/bibliotecapoo/Main.java
@@ -6,9 +6,12 @@ public class Main {
     }
 }
 
-//Clase base
-public abstract class RecursoBibliografico {
-    private String titulo;
+
+
+//HERENCIA
+// Clase base 
+abstract class RecursoBibliografico {
+    protected String titulo;
 
     public RecursoBibliografico(String titulo) {
         this.titulo = titulo;
@@ -18,9 +21,37 @@ public abstract class RecursoBibliografico {
         return titulo;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
     public abstract void mostrarDetalle();
 }
+
+// Subclase Libro
+class Libro extends RecursoBibliografico {
+    private String autor;
+    private String ISBN;
+    private boolean prestado;
+
+    public Libro(String titulo, String autor, String ISBN, boolean prestado) {
+        super(titulo);
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.prestado = prestado;
+    }
+
+    public void mostrarDetalle() {
+        System.out.println("Libro:");
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("ISBN: " + ISBN);
+        System.out.println("Prestado: " + (prestado ? "Sí" : "No"));
+    }
+}
+
+// Subclase Revista
+class Revista extends RecursoBibliografico {
+    private String ISSN;
+    private int anioPublicacion;
+    private int ejemplares;
+  //aun por completar
+}
+
+
--- bibliotecapoo/src/main/java/com/bibliotecapoo/model/Libro.java
+++
@@ -1,7 +0,0 @@
-package com.bibliotecapoo.model;
-
-public class Libro {
-    private String isbn;
-    private String título;
-    // … constructores, getters, setters, equals/hashCode, toString
-}
